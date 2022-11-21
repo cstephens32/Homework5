d10a85cd81c86a8f44bfad40fc4ffecce0775c32
Connor Stephens <connor.stephens@colostate.edu>
2022-11-21 10:36:52 -0700

    Setting up initial code

--- Homework5.R
+++ Homework5.R
@@ -1,1 +1,35 @@
-#Test
+## READ ME:
+##
+## Title: Homework 5
+## Author: Connor Stephens
+## Course: ERHS 535 
+## Email: connor.stephens@colostate.edu
+##
+## Year: 2022
+##
+## Language: R
+##
+## Description: This is a script for Homework 5. 
+##
+
+####  Set Directory ----
+
+#### Packages ####
+## If a package is installed, it will be loaded. If any 
+## are not, the missing package(s) will be installed 
+## from CRAN and then loaded.
+
+## Packages of interest
+packages = c("tidyverse")
+
+## Now load or install&load all
+package.check <- lapply(
+  packages,
+  FUN = function(x) {
+    if (!require(x, character.only = TRUE)) {
+      install.packages(x, dependencies = TRUE)
+      library(x, character.only = TRUE)}})
+
+
+## Load data ##
+
